5567c890d6b87182061e7ea8b5c548ad8c98d432
logic-fabric <lowic@logic-fabric.fr>
2023-06-05 07:54:08 +0200

    feature: build a PrimaryButton with emotion

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { PrimaryButton } from "./components/Button/Button";
+
 export function App() {
   return (
     <div>
       <h1>
         Design system with <em>Storybook</em> and <em>React</em>
       </h1>
+
+      <PrimaryButton>Primary button</PrimaryButton>
     </div>
   );
 }
---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,13 @@
+import styled from "@emotion/styled";
+
+export const PrimaryButton = styled.button`
+  padding: 1rem 4rem;
+  border: none;
+  border-radius: 0.5rem;
+
+  color: white;
+  font-size: 1.5rem;
+  font-weight: 700;
+
+  background: #1f7081;
+`;
